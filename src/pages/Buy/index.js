583155db1f794173e0dcb69a24076af9fbe99094
Caique Schinaider <caiqueschinaiderrufinoviana@gmail.com>
2024-10-22 23:31:17 -0300

    Definindo o escopo da pagina home

--- src/pages/Buy/index.js
+++ src/pages/Buy/index.js
@@ -5,13 +5,14 @@ import Card from "../../components/Card";
 import Header from "../../components/Header";
 import produtosMock from "../../utils/produtosMock";
 import Categoria from "../../components/Categoria";
+import Loading from "../../components/Loading";
 
 function Buy() {
   // Estado para a pesquisa e a categoria selecionada. A const "Pesquisa esta setada com os nosso Mock de produtos para que mesmo que não tenha nenhum valor de pesquisa, apareçam todos produtos"
   const [pesquisa, setPesquisa] = useState(produtosMock);
   const [categoriaSelecionada, setCategoriaSelecionada] = useState("");
 
-  // Função dispara pelo input de pesquisa para filtrar os produtos com base no valor digitado. O valor entra na const "valorPesquisa", sendo formatada para tudo em maiusculo para melhor compatibilidade. Logo em seguida a Const "produtosFiltrados", recebe o "produtosMock" com .filter, determinando que cada objeto dentro do array, seja determinado como (produto), usamos aplicamos algumas configurações (.categoria e .name) e logo em seguida o .inclues e dentro dos ( ) a const que recebeu o valor do input da pesquisa. Seria algo como "a categoria do produto(Toda maiuscula) é a que foi pesquisada?" Temos o .name e o .categoria separados por um || para caso um dos dois for true essa função irar setar no "setPesquisa" o valor que saiu na const "produtosFiltrados"
+  // Função dispara pelo input de pesquisa para filtrar os produtos com base no valor digitado. O valor entra na const "valorPesquisa", sendo formatada para tudo em maiusculo para melhor compatibilidade. Logo em seguida a Const "produtosFiltrados", recebe o "produtosMock" com .filter, determinando que cada objeto dentro do array, seja determinado como (produto), usamos aplicamos algumas configurações (.categoria e .name) e logo em seguida o .inclues e dentro dos ( ) a const que recebeu o valor do input da pesquisa. Seria algo como "a categoria do produto(Toda minuscula) é a que foi pesquisada?" Temos o .name e o .categoria separados por um || para caso um dos dois for true essa função irar setar no "setPesquisa" o valor que saiu na const "produtosFiltrados"
   function filtrarProdutos(evento) {
     const valorPesquisa = evento.target.value.toLowerCase();
 
@@ -26,6 +27,7 @@ function Buy() {
 
   // Função que é executada somente quando o usuario selecionar com click nos paragrafos. Ao clicar nos paragrafos, essa função é executada, assim o valor colocado no onClick, é setado na função entre os ( ), que logo depois declara uma const que armazena o "produtosMock" com .filter novamente, que ira definir os objetos (produtos), dentro dele como (produtos), em seguida fazemos que produto.categoria é identico a categoria, que no caso foi o valor do <p> clicado. Então setamos no "setPesquisa" com "produtosFiltrados" que contem exatamente a categoria clicada. setamos tbm no "setCategoriaSelecionada" o valor atual da categoria para uso futuros talvez
   function filtrarPorCategoria(categoria) {
+    carregar();
     const produtosFiltrados = produtosMock.filter(
       (produto) => produto.categoria === categoria
     );
@@ -33,15 +35,58 @@ function Buy() {
     setPesquisa(produtosFiltrados);
     setCategoriaSelecionada(categoria);
   }
+  const [Load, setLoad] = useState(false);
 
+  const carregar = (event) => {
+    setLoad(true);
+
+    setTimeout(() => {
+      setLoad(false);
+    }, 500);
+  };
   // Links de navegação por categoria
   const links = (
     <>
-      <p onClick={() => filtrarPorCategoria("Banheiro")}>Banheiro</p>
-      <p onClick={() => filtrarPorCategoria("Outros")}>Outros</p>
-      <p onClick={() => filtrarPorCategoria("Quarto")}>Quarto</p>
-      <p onClick={() => filtrarPorCategoria("Sala de Estar")}>Sala de Estar</p>
-      <p onClick={() => filtrarPorCategoria("Cozinha")}>Cozinha</p>
+      <p
+        onClick={() => {
+          filtrarPorCategoria("Banheiro");
+          carregar();
+        }}
+      >
+        Banheiro
+      </p>
+      <p
+        onClick={() => {
+          filtrarPorCategoria("Outros");
+          carregar();
+        }}
+      >
+        Outros
+      </p>
+      <p
+        onClick={() => {
+          filtrarPorCategoria("Quarto");
+          carregar();
+        }}
+      >
+        Quarto
+      </p>
+      <p
+        onClick={() => {
+          filtrarPorCategoria("Sala de Estar");
+          carregar();
+        }}
+      >
+        Sala de Estar
+      </p>
+      <p
+        onClick={() => {
+          filtrarPorCategoria("Cozinha");
+          carregar();
+        }}
+      >
+        Cozinha
+      </p>
     </>
   );
 
@@ -61,13 +106,17 @@ function Buy() {
           onChange={filtrarProdutos}
         />
       </div>
-
       {/* Esse "navegação" e "Need_AlgumaCategoria" servem para: O Need_AlgumaCategoria para mostrar a categoria atual (ainda não esta funcionando), ja o navegação, serve para puxar a const la de cima, e mostra la na tela (no componente categoria esta estruturado como ela se comporta no escopo). E no final de acordo com os produtos que foram filtrados e colocados dentro de "pesquisa" rodamos um map, declaramos que os produtos dentro se chamaram (produto), e como isso pegamos seu id, e todas informações, que vão para a props item, que puxa todos os dados do produtos */}
-      <Categoria navegação={links} Need_AlgumaCategoria={categoriaAtual}>
-        {pesquisa.map((produto) => (
-          <Card key={produto.id} item={produto} />
-        ))}
-      </Categoria>
+      {Load ? (
+        <Loading />
+      ) : (
+        <Categoria navegação={links} Need_AlgumaCategoria={categoriaAtual}>
+          {pesquisa.map((produto) => (
+            <Card key={produto.id} item={produto} />
+          ))}
+        </Categoria>
+      )}
+      )
     </section>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Header from "../../components/Header";
+
 import produtosMock from "../../utils/produtosMock";
 import styles from "./Home.module.css";
 
@@ -10,17 +11,35 @@ function Home() {
     <>
       <Header />
       <Banner />
+
       <section className={styles.Home}>
-        <h1>Você já conhece a nossa loja?</h1>
-        <p>
-          A <strong>Market Schin</strong> foi fundada com o compromisso de
-          fortalecer a relação entre consumidores e produtores.
-        </p>{" "}
-        <p>
-          {" "}
-          Atuamos como intermediários confiáveis, assegurando um transporte
-          eficiente, pagamentos seguros e a qualidade de nossos produtos!
-        </p>
+        <section>
+          <h1>Você já conhece a nossa loja?</h1>
+          <p>
+            A <strong>Market Schin</strong> foi fundada com o compromisso de
+            fortalecer a relação entre consumidores e produtores.
+          </p>{" "}
+          <p>
+            {" "}
+            Atuamos como intermediários confiáveis, assegurando um transporte
+            eficiente, pagamentos seguros e a qualidade de nossos produtos!
+          </p>
+        </section>
+        <inside>
+          <h2>Confira nossos serviços &#9660; &#9660; &#9660; </h2>
+          <nav>
+            <a href="#">
+              <p>Comprar</p>
+            </a>
+            <a href="#">
+              <p>Vender</p>
+            </a>
+            <a href="#">
+              <p>Ajuda</p>
+            </a>
+          </nav>
+          <div>Slide</div>
+        </inside>
       </section>
     </>
   );
