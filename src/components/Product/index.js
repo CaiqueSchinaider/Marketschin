b280a61dd2d39640559d754c1e6fe3091651e271
Caique Schinaider <caiqueschinaiderrufinoviana@gmail.com>
2024-10-29 21:23:45 -0300

    arrumei mais ou menos a responsividade

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -19,7 +19,7 @@ function Product() {
   return (
     <>
       <Header />
-      <Banner />
+
       {produtoatual ? (
         <div className={styles.Product}>
           <aside>
@@ -29,21 +29,39 @@ function Product() {
 
             <article>
               <h1>{produtoatual.name}</h1>
+              <p>
+                {Number.parseFloat(produtoatual.valor).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
               <button> Comprar </button> {/* Aqui vai parte da descrição */}
               <p>Avaliação: ⭐⭐⭐⭐☆ (4.5)</p>
+              <h2>Especificações</h2>
+              <p>{produtoatual.especificacoes}</p>
             </article>
           </aside>
 
           <section>
             {" "}
-            <h2>{produtoatual.name}</h2>
+            <h2>{produtoatual.name} </h2>
+            <h3>
+              <strong>
+                {Number.parseFloat(produtoatual.valor).toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </strong>
+            </h3>
             <p> {produtoatual.descricao}</p>
             <h3>Especificações </h3>
             <p>{produtoatual.especificacoes}</p>
           </section>
           <nav>
             <a>Ver relacionados</a>
+            <a>Ver carrinho</a>
             <a>Fechar carrinho</a>
+            <a>Voltar</a>
           </nav>
         </div>
       ) : (
