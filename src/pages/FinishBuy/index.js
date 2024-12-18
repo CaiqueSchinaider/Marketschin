b886cfda6c4f87f31bc76f504dc91fb4377bd5ba
Caique Schinaider <caiqueschinaiderrufinoviana@gmail.com>
2024-12-18 12:46:37 -0300

    finalizando pagina de Finalizar compra

--- src/pages/FinishBuy/index.js
+++ src/pages/FinishBuy/index.js
@@ -3,53 +3,73 @@ import styles from './FinishBuy.module.css';
 
 function FinishBuy() {
   const listCart = JSON.parse(localStorage.getItem('listcart'));
-
+  const CurrentBalance = listCart.reduce(
+    (current, initialvalue) => current + Number(initialvalue.valor),
+    0,
+  );
   if (listCart) {
     console.log(listCart);
   }
-  return listCart ? (
+  return listCart && CurrentBalance ? (
     <main className={styles.FinishPage}>
-      <section className={styles.Transaction}>
-        <div className={styles.CurrentBalance}>
-          {' '}
-          <h2>Saldo Atual</h2>
-          <p></p>
-        </div>
-
-        <img src="/pic/Back.png" alt="arrow"></img>
-
-        <div className={styles.PostPurchase}>
-          <h2>Saldo Pós</h2>
-        </div>
-      </section>
-
       <section className={styles.InfoCheck}>
         <div className={styles.ItensPurchased}>
-          <h3>Itens Comprados ({listCart.length})</h3>
-          {listCart.map((product) => (
-            <Link
-              to={`/produto/${product.id}`}
-              style={{ color: 'white', textDecoration: 'none' }}
-            >
-              <div key={product.id}>
-                <section>
-                  <p>{product.name}</p>
-                  <p style={{ color: '#9b0202' }}>
-                    {Number(product.valor).toLocaleString('pt-BR', {
-                      style: 'currency',
-                      currency: 'BRL',
-                    })}{' '}
-                  </p>
-                </section>
-                <img src={product.thumb} alt="imagem produto"></img>
-              </div>
-            </Link>
-          ))}
+          <h3>Itens no Carrinho ({listCart.length})</h3>
+          <section className={styles.ItensList}>
+            {listCart.map((product) => (
+              <Link
+                to={`/produto/${product.id}`}
+                style={{ color: 'white', textDecoration: 'none' }}
+              >
+                <div key={product.id}>
+                  <section>
+                    <p>{product.name}</p>
+                    <p
+                      style={{
+                        color: '#9b0202',
+                        fontFamily: 'Bebas Neue',
+                        fontSize: 'clamp(23px, 2.4vw, 3vw)',
+                      }}
+                    >
+                      {Number(product.valor).toLocaleString('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                      })}{' '}
+                    </p>
+                  </section>
+                  <img src={product.thumb} alt="imagem produto"></img>
+                </div>
+              </Link>
+            ))}
+          </section>
         </div>
 
-        <div className={styles.PriceInfo}> Informação do Preço</div>
-
-        <button> Finalizar Compra</button>
+        <div className={styles.PriceInfo}>
+          <h2> Itens </h2>
+          <div className={styles.priceList}>
+            <ul>
+              {listCart.map((product) => (
+                <li>
+                  {product.name} -{' '}
+                  {Number(product.valor).toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <nav>
+          <Link to="/comprar/all">
+            <button> Continuar Comprando</button>
+          </Link>
+          <Link to="/carrinho/none">
+            <button> Voltar para o carrinho</button>
+          </Link>
+          <button> Finalizar Compra</button>{' '}
+          {/* Levara para pagina de Finalização e pagamento (Precisa de um BackEnd)*/}
+        </nav>
       </section>
     </main>
   ) : (
