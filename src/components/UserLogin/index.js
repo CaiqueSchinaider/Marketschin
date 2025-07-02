fb6b31ee809bfe3322f400e3ddc3d665c5ee8766
Caique Schinaider <caiqueschinaiderrufinoviana@gmail.com>
2025-07-02 13:03:36 -0300

    v4

--- src/components/UserLogin/index.js
+++ src/components/UserLogin/index.js
@@ -40,93 +40,102 @@ function Logar() {
 
   // Verificação de email e senha
   const checkLogin = () => {
-    // Se a senha foi digitada
-    if (password === '') {
-      setHaveErrorPassword(false);
-      setInfoErrorPassword('A senha é obrigatoria');
-    }
-    // Se tudo estiver ok
-    else {
-      setHaveErrorPassword(true);
-    }
+    navigate('/home');
+    setParameterUtils({
+      nickname: 'test',
+      message: '',
+      destino: 'test@test.com',
+      senha: 'test',
+    });
 
-    /////////////////////////////////////////////////////////////////////////////
+    setSignal(false);
 
-    // Se o email foi digitado
-    if (email === '') {
-      setHaveErrorEmail(false);
-      setInfoErrorEmail('O email é obrigatorio!');
+    // // Se a senha fi digitada
+    // if (password === '') {
+    //   setHaveErrorPassword(false);
+    //   setInfoErrorPassword('A senha é obrigatoria');
+    // }
+    // // Se tudo estiver ok
+    // else {
+    //   setHaveErrorPassword(true);
+    // }
 
-      // Se contém @gmail.com no final do email, e se é maior que 11 caracteres
-    } else if (!email.endsWith('@gmail.com') || email.length < 11) {
-      setHaveErrorEmail(false);
-      setInfoErrorEmail('O email não é valido');
+    // /////////////////////////////////////////////////////////////////////////////
 
-      // Se tudo estiver ok
-    } else {
-      setHaveErrorEmail(true);
-    }
+    // // Se o email foi digitado
+    // if (email === '') {
+    //   setHaveErrorEmail(false);
+    //   setInfoErrorEmail('O email é obrigatorio!');
 
-    //signaliza o inicio das mensagens
-    setSignal(true);
+    //   // Se contém @gmail.com no final do email, e se é maior que 11 caracteres
+    // } else if (!email.endsWith('@gmail.com') || email.length < 11) {
+    //   setHaveErrorEmail(false);
+    //   setInfoErrorEmail('O email não é valido');
+
+    //   // Se tudo estiver ok
+    // } else {
+    //   setHaveErrorEmail(true);
+    // }
+
+    // //signaliza o inicio das mensagens
   };
 
-  useEffect(() => {
-    const handleLogin = async () => {
-      if (signal) {
-        if (!haveErrorPassword) {
-          setStatusErrorPassword('visible');
-          setErrorPassword('2px solid red');
-        } else {
-          setStatusErrorPassword('hidden');
-          setErrorPassword('none');
-        }
-        if (!haveErrorEmail) {
-          setStatusErrorEmail('visible');
-          setErrorEmail('2px solid red');
-        } else {
-          setStatusErrorEmail('hidden');
-          setErrorEmail('none');
-        }
-        if (haveErrorEmail && haveErrorPassword) {
-          const usersData = async () => {
-            const data = await getDocs(userCollectionRef);
-            const infoData = data.docs.map((doc) => ({
-              ...doc.data(),
-              id: doc.id,
-            }));
-            return infoData;
-          };
+  // useEffect(() => {
+  //   const handleLogin = async () => {
+  //     if (signal) {
+  //       if (!haveErrorPassword) {
+  //         setStatusErrorPassword('visible');
+  //         setErrorPassword('2px solid red');
+  //       } else {
+  //         setStatusErrorPassword('hidden');
+  //         setErrorPassword('none');
+  //       }
+  //       if (!haveErrorEmail) {
+  //         setStatusErrorEmail('visible');
+  //         setErrorEmail('2px solid red');
+  //       } else {
+  //         setStatusErrorEmail('hidden');
+  //         setErrorEmail('none');
+  //       }
+  //       if (haveErrorEmail && haveErrorPassword) {
+  //         const usersData = async () => {
+  //           const data = await getDocs(userCollectionRef);
+  //           const infoData = data.docs.map((doc) => ({
+  //             ...doc.data(),
+  //             id: doc.id,
+  //           }));
+  //           return infoData;
+  //         };
 
-          const infoData = await usersData();
-          const logFindEmail = infoData.find((users) => users.email === email);
+  //         const infoData = await usersData();
+  //         const logFindEmail = infoData.find((users) => users.email === email);
 
-          if (logFindEmail && logFindEmail.senha == password) {
-            // Se tudo estiver ok
-            navigate('/home');
-            setParameterUtils({
-              nickname: logFindEmail.nickname,
-              message: '',
-              destino: email,
-              senha: password,
-            });
-          } else if (!logFindEmail) {
-            console.log('Email é', logFindEmail);
-            // Se não, verifica se existe uma conta com esse email, ou se a senha ta errada
-            setInfoErrorEmail('Essa conta não existe!');
-            setStatusErrorEmail('visible');
-            setErrorEmail('2px solid red');
-          } else if (logFindEmail.password !== password) {
-            setInfoErrorPassword('Senha incorreta');
-            setStatusErrorPassword('visible');
-            setErrorPassword('2px solid red');
-          }
-        }
-      }
-    };
-    handleLogin();
-    setSignal(false);
-  }, [signal]);
+  //         if (logFindEmail && logFindEmail.senha == password) {
+  //           // Se tudo estiver ok
+  //           navigate('/home');
+  //           setParameterUtils({
+  //             nickname: logFindEmail.nickname,
+  //             message: '',
+  //             destino: email,
+  //             senha: password,
+  //           });
+  //         } else if (!logFindEmail) {
+  //           console.log('Email é', logFindEmail);
+  //           // Se não, verifica se existe uma conta com esse email, ou se a senha ta errada
+  //           setInfoErrorEmail('Essa conta não existe!');
+  //           setStatusErrorEmail('visible');
+  //           setErrorEmail('2px solid red');
+  //         } else if (logFindEmail.password !== password) {
+  //           setInfoErrorPassword('Senha incorreta');
+  //           setStatusErrorPassword('visible');
+  //           setErrorPassword('2px solid red');
+  //         }
+  //       }
+  //     }
+  //   };
+  //   handleLogin();
+  //   setSignal(false);
+  // }, [signal]);
 
   return (
     <section className={styles.LoginData}>
